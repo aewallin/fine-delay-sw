f6e73254a00244b59d7bb5d2d72bc5badb26d2cd
Alessandro Rubini <rubini@gnudd.com>
2013-04-30 13:17:57 +0200

    kernel/calibration and doc: new calibration is there
    The new calibration is now there, but some details are still missing,
    like using the DAC value for the VCXO and validating against the
    installed gateware.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- kernel/calibration.c
+++ kernel/calibration.c
@@ -15,6 +15,7 @@
 #include <linux/firmware.h>
 #include <linux/jhash.h>
 #include "fine-delay.h"
+#include "../sdb-lib/libsdbfs.h"
 
 /* At factory config time, it's possible to load a file and/or write eeprom */
 static char *calibration_load;
@@ -71,48 +72,113 @@ static struct fd_calibration fd_calib_default = {
 	.magic = 0xf19ede1a,
 	.version = 3,
 	.date = 0x20130427,
-	/* .. FIXME .. */
+	.frr_poly = { -165202LL, -29825595LL, 3801939743082LL },
+	.zero_offset = { -38186, -38155, -38147, -38362 },
+	.tdc_zero_offset = 127500,
+	.vcxo_default_tune = 41711,
 };
 
+/* sdbfs-related function */
+static int fd_read_calibration_eeprom(struct fmc_device *fmc,
+				      struct fd_calibration *calib)
+{
+	int i, ret;
+	static struct sdbfs fs;
 
+	fs.data = fmc->eeprom;
+	fs.datalen = fmc->eeprom_len;
+
+	/* Look for sdb entry point at powers of 2 and onwards */
+	for (i = 0x40; i < 0x1000; i *= 2) {
+		fs.entrypoint = i;
+		ret = sdbfs_dev_create(&fs, 0);
+		if (ret == 0)
+			break;
+	}
+	if (ret)
+		return ret;
+
+	/* Open "cali" as a device id, vendor is "FileData" -- big endian */
+	ret = sdbfs_open_id(&fs, 0x61746144656c6946LL, 0x696c6163);
+	if (ret)
+		return ret;
+	ret = sdbfs_fread(&fs, 0, (void *)calib, sizeof(*calib));
+	sdbfs_dev_destroy(&fs);
+	return ret;
+}
+
+/* This is the only thing called by outside */
 int fd_handle_eeprom_calibration(struct fd_dev *fd)
 {
 	struct fd_calibration *calib;
 	struct device *d = fd->fmc->hwdev;
-	//u32 hash;
+	int i;
+	u32 hash, horig;
 
 	/* Retrieve and validate the calibration */
 	calib = &fd->calib;
 
-	/* FIXME: run SDB on the eeprom */
-	dev_warn(d, "Calibration NOT yet read from eeprom\n");
+	i = fd_read_calibration_eeprom(fd->fmc, calib);
+	if (i != sizeof(*calib))
+		dev_warn(d, "Calibration NOT read from eeprom (got %i)\n", i);
 
 	if (calibration_check)
 		dumpstruct("Calibration data from eeprom:", calib,
 			   sizeof(*calib));
 
-	/* FIXME: verify hash */
+	/* Verify hash (used later) */
+	horig = be32_to_cpu(calib->hash);
+	calib->hash = 0;
+	hash = jhash(calib, sizeof(*calib), 0);
 
 	/* FIXME: validate with gateware version */
 
-	if (calibration_load)
+	if (calibration_load) {
 		fd_i2c_load_calib(fd, calib, calibration_load);
+		hash = horig; /* whatever it is */
+	}
 
-	/* FIXME: convert endianneess */
+	/* convert endianneess */
+	calib->magic = be32_to_cpu(calib->magic);
+	calib->size = be16_to_cpu(calib->size);
+	calib->version = be16_to_cpu(calib->version);
+	calib->date = be32_to_cpu(calib->date);
+	for (i = 0; i < ARRAY_SIZE(calib->frr_poly); i++)
+		calib->frr_poly[i] = be64_to_cpu(calib->frr_poly[i]);
+	for (i = 0; i < ARRAY_SIZE(calib->zero_offset); i++)
+		calib->zero_offset[i] = be32_to_cpu(calib->zero_offset[i]);
+	calib->tdc_zero_offset = be32_to_cpu(calib->tdc_zero_offset);
+	calib->vcxo_default_tune = be32_to_cpu(calib->vcxo_default_tune);
 
 	if (calibration_default) {
 		dev_info(d, "Overriding with default calibration\n");
 		*calib = fd_calib_default;
+		hash = horig; /* whatever it is */
 	}
 
 	if (calibration_check) {
-		/* FIXME: dump human-readable values */
+		/* dump human-readable values */
+		dev_info(d, "calib: magic 0x%08x, date %08x\n", calib->magic,
+			calib->date);
+		dev_info(d, "calib: version %i\n", calib->version);
+		for (i = 0; i < ARRAY_SIZE(calib->frr_poly); i++)
+			dev_info(d, "calib: poly[%i] = %lli\n", i,
+				 (long long)calib->frr_poly[i]);
+		for (i = 0; i < ARRAY_SIZE(calib->zero_offset); i++)
+			dev_info(d, "calib: offset[%i] = %li\n", i,
+				 (long)calib->zero_offset[i]);
+		dev_info(d, "calib: tdc_offset %i\n", calib->tdc_zero_offset);
+		dev_info(d, "calib: vcxo %i\n", calib->vcxo_default_tune);
 	}
 
-	/* FIXME: recreate hash */
+	if (hash != horig) {
+		dev_err(d, "Calibration hash %08x is wrong (expected %08x)\n",
+			hash, horig);
+		return -EINVAL;
+	}
 
 	if (calibration_save) {
-		/* FIXME:  save to eeprom */
+		/* FIXME:  save to eeprom: re-convert endianness and hash */
 		dev_warn(d, "Saving is not supported\n");
 	}
 
